63ba881100e1634c20aefb195211e10327ac9383
AndreyChepik <and.chepik2016@gmail.com>
2021-05-13 03:56:15 +0300

    paginator added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)  # три поста на каждую страницу
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # return first page if page is not an integer
+        posts = paginator.page(1)
+    except EmptyPage:
+        # return last page if page is out of range
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year, publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
