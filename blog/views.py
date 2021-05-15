ea195cd0a89bf0fba09888a0eb6a80bc5b30715e
AndreyChepik <and.chepik2016@gmail.com>
2021-05-15 03:40:03 +0300

    email functionality added

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,53 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
 
 
-def post_list(request):
-    posts_list = Post.published.all()
-    paginator = Paginator(posts_list, 3)  # три поста на каждую страницу
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # return first page if page is not an integer
-        posts = paginator.page(1)
-    except EmptyPage:
-        # return last page if page is out of range
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+def post_share(request, post_id):
+    # выбираем пост с нужным id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        # если метод запроса post, то работаем с введенными данными
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # поля формы прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"
+            f"{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+
+# def post_list(request):
+#     posts_list = Post.published.all()
+#     paginator = Paginator(posts_list, 3)  # три поста на каждую страницу
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # return first page if page is not an integer
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # return last page if page is out of range
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
