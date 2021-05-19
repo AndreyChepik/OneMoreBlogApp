63b0465689ce315a2208610eddbcfdf42bb26599
AndreyChepik <and.chepik2016@gmail.com>
2021-05-19 05:11:32 +0300

    Some forms and Comment model added

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,6 @@ class PublishedManager(models.Manager):
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
-
         ('published', 'Published')
     )
     title = models.CharField(max_length=250)
@@ -43,3 +42,17 @@ class Post(models.Model):
     published = PublishedManager() #кастомный менеджер
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
